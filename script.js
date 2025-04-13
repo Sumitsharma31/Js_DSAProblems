4e274aab931d668dd7375a3845a59b226170bcf8
sumit sharma <ssumit20kr@gmail.com>
2025-04-13 19:10:55 +0530

    2ndMethod to check palindrome

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // JavaScript Program to Check for Palindrome Number
-//Approach 1
-function palindromeCheck(num) {
+//Approach 1 Using String Reversal
+
+/*function palindromeCheck(num) {
     let numStr = num.toString();
     let result = numStr.split('').reverse().join('');
     return numStr === result;
 }
 
 console.log(palindromeCheck(121)); 
-console.log(palindromeCheck(123)); 
+console.log(palindromeCheck(123)); */
+
+// Approach 2: Using Array Every() Method
+
+function palindromeCheck(number) {
+    let numStr = number.toString();
+    let numArr = numStr.split('');
+    console.log(numArr);
+    
+    return numArr.every((num, index) =>
+        {console.log(num,index);
+            num === numArr[numArr.length - 1 - index]});
+}
+
+// Checking the number is Palindrome
+console.log(palindromeCheck(121));
+// console.log(palindromeCheck(12321));
+// console.log(palindromeCheck(12345)); 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(palindromeCheck(12321));
+// console.log(palindromeCheck(12345)); 
