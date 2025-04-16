8876ac09735c9471255e0fb84629517fdaf8e551
sumit sharma <ssumit20kr@gmail.com>
2025-04-16 21:35:24 +0530

    fund Power of a Num By using loop

---
+++ findPowerofNum.js
@@ -0,0 +1,15 @@
+// JavaScript Program to Compute Power of a Number
+// 1 Using JavaScript Loops
+let n = 5
+
+// Power input
+let power = 3
+
+// Result variable
+let num = 1;
+for (let i = 0; i < power; ++i) {
+    num = num * n;
+}
+
+// Display output
+console.log(num);
