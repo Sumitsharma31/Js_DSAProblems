38c3aa58b72a58292956d0eb3980e9964e7bf04a
sumit sharma <ssumit20kr@gmail.com>
2025-04-16 21:37:26 +0530

    Bu uing recursive Function in js

--- findPowerofNum.js
+++ findPowerofNum.js
@@ -1,15 +1,32 @@
 // JavaScript Program to Compute Power of a Number
 // 1 Using JavaScript Loops
-let n = 5
+
+// let n = 5
+
+// // Power input
+// let power = 3
+
+// // Result variable
+// let num = 1;
+// for (let i = 0; i < power; ++i) {
+//     num = num * n;
+// }
+
+// // Display output
+// console.log(num);
+
+
+//2 Recursive function to compute Power
+function pow(n, p) {
+    if (p == 1) return n;
+    return n * pow(n, p - 1);
+}
+
+// Base number input
+let n = 8;
 
 // Power input
 let power = 3
 
-// Result variable
-let num = 1;
-for (let i = 0; i < power; ++i) {
-    num = num * n;
-}
-
 // Display output
-console.log(num);
+console.log(pow(n, power));
