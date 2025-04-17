2324e279f4ad994907b8ce95f2cabd2ef95981a2
sumit sharma <ssumit20kr@gmail.com>
2025-04-17 22:20:44 +0530

    recrsive Function to find factorial

---
+++ recursion prob/factOfNum.js
@@ -0,0 +1,12 @@
+//  Factorial of a Number
+function factorial(n) {
+    // Base case: if n is 0 or 1, return 1
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+  
+    // Recursive case: n! = n * (n-1)!
+    return n * factorial(n - 1);
+  }
+  
+  console.log(factorial(5));
