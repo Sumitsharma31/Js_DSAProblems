253b2d7d1f24277959dfc8361802625cf032c506
sumit sharma <ssumit20kr@gmail.com>
2025-04-20 23:06:46 +0530

    array Methods

---
+++ Array/accexInArr.js
@@ -0,0 +1,16 @@
+const arr=new Array(5,4,8,6);
+
+// console.log(arr);
+// These are the following ways to Access Elements in an Array:
+
+// 1. Using Square Bracket Notation
+
+let firstV=arr[0];
+console.log(arr);
+const newArr=[];
+// 2. Using forEach Loop
+for(let i=0;i<arr.length;i++){
+
+newArr[i]=arr[i]
+}
+console.log(newArr,"Copy");
