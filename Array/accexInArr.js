ee5baf675cc7e33b5971907c097b55c6d1373639
sumit sharma <ssumit20kr@gmail.com>
2025-04-21 22:35:44 +0530

    map Method

--- Array/accexInArr.js
+++ Array/accexInArr.js
@@ -6,9 +6,16 @@ const arr=new Array(5,4,8,6);
 // 1. Using Square Bracket Notation
 console.log(arr[3]);
 
-const newArr=[];
+
 // 2. Using forEach Loop
-const a = [100, 200, 300, 400, 500];
-a.forEach((item, index,Array) => {
-    console.log(item );
-});
+
+// const a = [100, 200, 300, 400, 500];
+// a.forEach((item, index,Array) => {
+//     console.log(item );
+// });
+
+// 3. Using map method 
+arr.map((item,index)=>{
+    console.log(item);
+    
+})
