e4053029399618d1b3b0210b4038ec3ddc0ac950
sumit sharma <ssumit20kr@gmail.com>
2025-04-22 21:58:29 +0530

    Binart Search to find first index

--- searching/findIndex.js
+++ searching/findIndex.js
@@ -1,20 +1,74 @@
 // Method 1: Using Linear Search
-arr = [1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 8, 8, 9];
+
+// arr = [1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 8, 8, 9];
+
+// // Target element
+// target = 5;
+
+// // Iterate till length of array
+// for (let i = 0; i < arr.length; ++i) {
+
+//     // If target found return and exit program
+//     if (arr[i] === target) {
+//         console.log(
+//             "First index of " + target + " is: " + i
+//         );
+//         return;
+//     }
+// }
+
+// // If not found display output
+// console.log(target + " is not present in the given array");
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2: Using Binary Search
+const arr = [1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 8, 8, 9];
 
 // Target element
-target = 5;
+const target = 5;
+let left = 0;
+let right = arr.length - 1;
+let outputIndex = -1;
 
-// Iterate till length of array
-for (let i = 0; i < arr.length; ++i) {
+// Using binary search
+while (left <= right) {
+    
+    let mid = Math.floor((left + right) / 2);
 
-    // If target found return and exit program
-    if (arr[i] === target) {
-        console.log(
-            "First index of " + target + " is: " + i
-        );
-        return;
+    // If found search left half for 
+    // First occerrence
+    if (target === arr[mid]) {
+
+        // Store first occurrence index
+        outputIndex = mid;
+        right = mid - 1;
     }
+
+    // If target is smallar discard right half
+    else if (target < arr[mid]) {
+        right = mid - 1;
+    }
+
+    // If target is greater discard left half
+    else
+        left = mid + 1;
 }
 
 // If not found display output
-console.log(target + " is not present in the given array");
+if (outputIndex === -1)
+    console.log(target + " is not present in the given");
+else {
+    console.log(
+        "First index of " + target + " is: " + outputIndex
+    );
+}
