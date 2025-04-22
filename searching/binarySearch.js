d4b66ffdfdaeffbae71354ef121ce02d34f662dd
sumit sharma <ssumit20kr@gmail.com>
2025-04-22 21:49:40 +0530

    Iterative Approach

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -1,37 +1,88 @@
-let recursiveFunction = function (arr, x, start, end) {
+// 1. Recursive Approach
 
-    // Base Condition
-    if (start > end) return false;
+// let recursiveFunction = function (arr, x, start, end) {
 
-    // Find the middle index
-    let mid = Math.floor((start + end) / 2);
+//     // Base Condition
+//     if (start > end) return false;
 
-    // Compare mid with given key x
-    if (arr[mid] === x) return true;
+//     // Find the middle index
+//     let mid = Math.floor((start + end) / 2);
 
-    // If element at mid is greater than x,
-    // search in the left half of mid
-    if (arr[mid] > x)
-        return recursiveFunction(arr, x, start, mid - 1);
-    else
+//     // Compare mid with given key x
+//     if (arr[mid] === x) return true;
 
-        // If element at mid is smaller than x,
-        // search in the right half of mid
-        return recursiveFunction(arr, x, mid + 1, end);
-}
+//     // If element at mid is greater than x,
+//     // search in the left half of mid
+//     if (arr[mid] > x)
+//         return recursiveFunction(arr, x, start, mid - 1);
+//     else
 
-// Driver code
-let arr = [1,2, 3, 5, 7, 8, 9];
-let x = 5;
+//         // If element at mid is smaller than x,
+//         // search in the right half of mid
+//         return recursiveFunction(arr, x, mid + 1, end);
+// }
 
-if (recursiveFunction(arr, x, 0, arr.length - 1)) {
-    console.log("Element found!");
-}
-else { console.log("Element not found!"); }
+// // Driver code
+// let arr = [1,2, 3, 5, 7, 8, 9];
+// let x = 5;
 
-x = 2;
+// if (recursiveFunction(arr, x, 0, arr.length - 1)) {
+//     console.log("Element found!");
+// }
+// else { console.log("Element not found!"); }
 
-if (recursiveFunction(arr, x, 0, arr.length - 1)) {
-    console.log("Element found!");
-}
-else { console.log("Element not found!"); }
+// x = 2;
+
+// if (recursiveFunction(arr, x, 0, arr.length - 1)) {
+//     console.log("Element found!");
+// }
+// else { console.log("Element not found!"); }
+
+// 2. Iterative Approach
+// Iterative function to implement Binary Search
+
+
+// let iterativeFunction = function (arr, x) {
+
+//     let start = 0, end = arr.length - 1;
+
+//     // Iterate while start not meets end
+//     while (start <= end) {
+
+//         // Find the mid index
+//         let mid = Math.floor((start + end) / 2);
+
+//         // If element is present at 
+//         // mid, return True
+//         if (arr[mid] === x) return true;
+
+//         // Else look in left or 
+//         // right half accordingly
+//         else if (arr[mid] < x)
+//             start = mid + 1;
+//         else
+//             end = mid - 1;
+//     }
+
+//     return false;
+// }
+
+// // Driver code
+// let arr = [1, 3, 5, 7, 8, 9];
+// let x = 5;
+
+// if (iterativeFunction(arr, x, 0, arr.length - 1)) {
+//     console.log("Element found!");
+// }
+// else {
+//     console.log("Element not found!");
+// }
+
+// x = 8;
+
+// if (iterativeFunction(arr, x, 0, arr.length - 1)) {
+//     console.log("Element found!");
+// }
+// else {
+//     console.log("Element not found!");
+// }
