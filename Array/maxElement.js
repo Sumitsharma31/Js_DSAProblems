1b9ecf02793f3aa6e9c12656f1698c32dca3fa0e
sumit sharma <ssumit20kr@gmail.com>
2025-04-23 22:07:12 +0530

    forEach method to find max element

--- Array/maxElement.js
+++ Array/maxElement.js
@@ -5,7 +5,18 @@
 // console.log(Math.max(...a));
 // console.log([...a]);
 
-// 3.Using sort() Method
+// 2.Using sort() Method
 const arr = [22, 65, 1, 39];
 arr.sort((a, b) => b - a);
 console.log(arr[0]);
+
+
+// 3.Using the forEach Method
+let a = [1, 2, 3, 4, 5];
+let max = a[0];
+a.forEach(num => {
+    if (num > max) {
+        max = num;
+    }
+});
+console.log(max); 
