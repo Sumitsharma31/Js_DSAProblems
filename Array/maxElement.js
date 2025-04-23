3d5f58bdbf47576e5cd1909787218901c244f424
sumit sharma <ssumit20kr@gmail.com>
2025-04-23 22:11:21 +0530

    By using reduse method

--- Array/maxElement.js
+++ Array/maxElement.js
@@ -6,17 +6,29 @@
 // console.log([...a]);
 
 // 2.Using sort() Method
-const arr = [22, 65, 1, 39];
-arr.sort((a, b) => b - a);
-console.log(arr[0]);
+
+// const arr = [22, 65, 1, 39];
+// arr.sort((a, b) => b - a);
+// console.log(arr[0]);
 
 
 // 3.Using the forEach Method
-let a = [1, 2, 3, 4, 5];
-let max = a[0];
-a.forEach(num => {
-    if (num > max) {
-        max = num;
-    }
+
+// let a = [1, 2, 3, 4, 5];
+// let max = a[0];
+// a.forEach(num => {
+//     if (num > max) {
+//         max = num;
+//     }
+// });
+// console.log(max); 
+
+
+// 4.Using reduce() Method
+const array = [22, 65, 1, 39];
+const ans = array.reduce(function (a, b) {
+    console.log(a,b);
+    
+    return (a > b) ? a : b
 });
-console.log(max); 
+console.log(ans);
