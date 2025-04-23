1e3f32d22ed737de50e5c7c44c89a8414c56892b
sumit sharma <ssumit20kr@gmail.com>
2025-04-23 22:04:21 +0530

    short method to find max element

--- Array/maxElement.js
+++ Array/maxElement.js
@@ -1,6 +1,11 @@
 // Max Element in an Array
 // 1. Using the Spread Operator
 
-const a = [22, 65, 1, 39];
-console.log(Math.max(...a));
+// const a = [22, 65, 1, 39];
+// console.log(Math.max(...a));
 // console.log([...a]);
+
+// 3.Using sort() Method
+const arr = [22, 65, 1, 39];
+arr.sort((a, b) => b - a);
+console.log(arr[0]);
