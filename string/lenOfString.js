c3ef352b7e2c0b1b89369b97e3b4a5ce308df959
sumit sharma <ssumit20kr@gmail.com>
2025-04-25 23:34:25 +0530

    string concatenation

--- string/lenOfString.js
+++ string/lenOfString.js
@@ -3,3 +3,11 @@ let s = 'JavaScript';
 let len = s.length;
 
 console.log("String Length: " + len);
+
+// String Concatenation
+let s1 = 'Java';
+let s2 = 'Script';
+let res = s1 + s2;
+
+console.log("Concatenated String: " + res);
+
