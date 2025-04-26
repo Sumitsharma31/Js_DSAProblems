8aad3a0b9b6df011efc6136cbc7d4dde5a8accba
sumit sharma <ssumit20kr@gmail.com>
2025-04-26 22:59:35 +0530

    by using reveerse Technique

--- string/palindromeStr.js
+++ string/palindromeStr.js
@@ -1,13 +1,37 @@
 // 1: Using for loop
+
+// function isPalindrome(str) {
+//     let j = str.length - 1
+//     for (let i = 0; i < str.length / 2; i++) {
+//         if (str[i] != str[j]) {
+//             return false;
+//         }
+//         j--;
+//     }
+//     return true;
+// }
+
+// let str1 = "racecar";
+// let str2 = "nitin";
+// let str3 = "Rama";
+
+// console.log(isPalindrome(str1));
+// console.log(isPalindrome(str2));
+// console.log(isPalindrome(str3));
+
+
+// 2: Checking the String from Last
+
 function isPalindrome(str) {
-    let j = str.length - 1
-    for (let i = 0; i < str.length / 2; i++) {
-        if (str[i] != str[j]) {
-            return false;
-        }
-        j--;
+    let rev = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
     }
-    return true;
+    if (rev == str) {
+        return true
+    } else {
+        return false;
+    }
 }
 
 let str1 = "racecar";
