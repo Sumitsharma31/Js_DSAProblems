b518c2789b0d3136e64b7b91e7c8bf62bb99e67c
sumit sharma <ssumit20kr@gmail.com>
2025-04-28 22:36:52 +0530

    union method Using concat and filter

--- Array/unionArrey.js
+++ Array/unionArrey.js
@@ -3,5 +3,19 @@ const a1 = [1, 2, 3];
 const a2 = [3, 4, 5];
 
 // Union of arrays
-const union = [...new Set([...a1, ...a2])];
+let union = [...new Set([...a1, ...a2])];
 console.log(union); 
+
+
+// 2.Using push() Method
+
+
+a1.push(...a2);
+ union = [...new Set(a1)];
+console.log(union); 
+
+
+
+// Using concat() and filter()
+let newUnion = a1.concat(a2).filter((value, index, arr) => arr.indexOf(value) === index);
+console.log(newUnion); 
