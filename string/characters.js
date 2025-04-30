9d38102d204122e246abd4f23babbf9aeded9ad8
sumit sharma <ssumit20kr@gmail.com>
2025-04-30 23:12:51 +0530

    charat method

---
+++ string/characters.js
@@ -0,0 +1,11 @@
+// How to Get Character of Specific Position using JavaScript ?
+
+// Method 1: Using String charAt() Method
+let str = "Welcome to GeeksforGeeks";
+
+let index = 11;
+
+let ctr = str.charAt(index);
+
+console.log("Character at " + index 
+    + "th position is: " + ctr);
