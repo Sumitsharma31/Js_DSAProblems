091cfe057a8e3f1ae833700c427984d2abb6540a
sumit sharma <ssumit20kr@gmail.com>
2025-04-30 23:18:14 +0530

    substr method

--- string/characters.js
+++ string/characters.js
@@ -1,11 +1,23 @@
 // How to Get Character of Specific Position using JavaScript ?
 
 // Method 1: Using String charAt() Method
+
+// let str = "Welcome to GeeksforGeeks";
+
+// let index = 11;
+
+// let ctr = str.charAt(index);
+
+// console.log("Character at " + index
+//     + "th position is: " + ctr);
+
+// 2. Using String substr() Method
+
 let str = "Welcome to GeeksforGeeks";
 
 let index = 11;
 
-let ctr = str.charAt(index);
+let ctr = str.substr(index, 1);
 
-console.log("Character at " + index 
+console.log("Character at " + index
     + "th position is: " + ctr);
