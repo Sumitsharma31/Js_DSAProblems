a5e6a7c4d52a41829cb6d9930c914f185187e88b
sumit sharma <ssumit20kr@gmail.com>
2025-05-08 23:35:36 +0530

    remove duplicates from an array

---
+++ Array/removeDublicate.js
@@ -0,0 +1,6 @@
+// How do you remove duplicates from an array?
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); 
--- Linkedlist/implition.js
+++ Linkedlist/implition.js
@@ -1,13 +1,5 @@
 // Implementation of LinkedList in Javascript
 
-// The Node class defines a structure with a value to store data and a next pointer initialized to null, representing the next node in the linked list.
-// The constructor takes a value parameter to set the node’s data and creates a node with no link to the next node initially.
-// Implementation of a linked list
-// 1. Creaing a Linked List
-// To create a simple linked list in JavaScript, the provided code defines a LinkedList class and a Node class to represent individual elements.
-
-
-
 
 class Node{
     constructor(value)
